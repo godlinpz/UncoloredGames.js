a4499b90b18f58681a53957c7d198021eefc6370
Dmitry Pavlichenko <Dmitry.Pavlichenko@macys.com>
2021-12-31 06:29:35 +0300

    Tests for KeyboardInput

---
+++ tests/input/KeyboardInput.test.js
@@ -0,0 +1,38 @@
+import KeyboardInput from '../../input/KeyboardInput.mjs';
+import jest from 'jest-mock';
+
+
+describe('Keyboard input', () => {
+
+    function init()
+    {
+        const callback = jest.fn();
+        return {
+            callback, 
+            ki: new KeyboardInput({
+                canvas: {
+                    addEventListener: jest.fn(),
+                    removeEventListener: jest.fn(),
+                },
+            }),
+        };
+    }
+
+    test('triggers key down with button specified', () => {
+        const {callback, ki} = init();
+
+        ki.on('keydown_Space', callback);
+        ki.handleDomEvent('keydown', {code: 'Space'});
+
+        expect(callback).toHaveBeenCalled();
+    });
+
+    test('triggers key up with button specified', () => {
+        const {callback, ki} = init();
+
+        ki.on('keyup_Space', callback);
+        ki.handleDomEvent('keyup', {code: 'Space'});
+
+        expect(callback).toHaveBeenCalled();
+    });
+});
